90b8929fcb9e0605c8600893debd1cb47f03401d
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-12-02 17:12:09 +0530

    booking model created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import Bus from "./Models/Buses.js";
-// import Booking from "./Models/Booking.js";
+import Booking from "./Models/Booking.js";
 dotenv.config()
 const app=express()
 app.use(express.json());
@@ -15,6 +15,8 @@ const connectMongoDB= async()=>{
     }
 }
 connectMongoDB();
+
+//bus model
 app.post("/api/buses",async(req , res)=>{
     const { busno,seats, busname}=req.body;
     const bus=new Bus({ busno,seats, busname})
@@ -38,7 +40,7 @@ app.post("/api/buses",async(req , res)=>{
 
 app.get("/api/buses",async(req,res)=>{
     const allbuses=await Bus.find()
-    res.status(200).json({
+   return res.status(200).json({
         success:true,
         data:allbuses,
         message:"all buses found successfully"
@@ -84,9 +86,89 @@ app.patch("/api/buses/:_id", async (req,res)=>{
 app.delete("/api/buses/:_id", async(req,res)=>{
     const {_id}=req.params
     await Bus.deleteOne({_id:_id})
-    res.status(204).json({
+  return  res.status(204).json({
         success:true,
         message:"data deleted successfully"
+    })
+
+
+})
+
+
+//booking models
+
+app.post("/api/tickets",async(req , res)=>{
+    const { passe_name,  passe_age , from ,to }=req.body;
+    const booking = new Booking({ passe_name,  passe_age , from ,to})
+    try{
+        const savedbooking= await booking.save()
+       return res.status(201).json({
+        success:true,
+        data:savedbooking,  
+        message:'booking data post successfully'
+    })
+    }
+    catch(err){
+     return res.json({
+            success:false,
+            message:err.message
+        })
+    }
+
+
+})
+
+app.get("/api/tickets",async(req,res)=>{
+    const allbookings=await Booking.find()
+   return res.status(200).json({
+        success:true,
+        data:allbookings,
+        message:"all bookimgs found successfully"
+    })
+})
+
+app.put("/api/tickets/:_id", async (req,res)=>{
+    const {_id}=req.params
+    const { passe_name,  passe_age , from ,to}=req.body;
+   await Booking.updateOne({_id:_id} , {$set:{
+  passe_name:passe_name, passe_age:passe_age, from:from , to:to
+   }})
+    
+    const updatedbooking=await Booking.findOne({_id:_id})
+    return res.status(201).json({
+        success:true,
+        data:updatedbooking,
+        message:"data updated successfully"
+    })
+    
+   
+
+    
+ 
+})
+
+app.patch("/api/tickets/:_id", async (req,res)=>{
+    const {_id}=req.params
+    const {to}=req.body;
+   await Booking.updateOne({_id:_id} , {$set:{
+     to:to
+   }})
+    
+    const updatedbooking=await Booking.findOne({_id:_id})
+    return res.status(201).json({
+        success:true,
+        data:updatedbooking,
+        message:"data updated successfully"
+    })
+    
+})
+
+app.delete("/api/tickets/:_id", async(req,res)=>{
+    const {_id}=req.params
+    await Booking.deleteOne({_id:_id})
+  return  res.status(204).json({
+        success:true,
+          message:"data deleted successfully"
     })
 
 
