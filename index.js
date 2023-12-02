0a1e60c353add1067e721776f56553f8e2fc0695
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-12-02 15:38:48 +0530

    mongodb connected succesfully

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+dotenv.config()
 const app=express()
-const PORT=5000
+
+const connectMongoDb=async()=>{
+    const connect=mongoose.connect(process.env.MONGODB_URI)
+    if(connect){
+        console.log("mongodb connected successfully")
+    }
+
+}
+connectMongoDb();
+
+const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log(`server is running in port ${PORT}`)
 })
